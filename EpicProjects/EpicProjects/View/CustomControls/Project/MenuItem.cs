d86fed1bd748922b323c1d78f823c080c9b89351
RemyKaloustian <remy.kaloustian@gmail.com>
2016-02-28 19:48:58 +0100

    Begining the custom project sketch

---
+++ EpicProjects/EpicProjects/View/CustomControls/Project/MenuItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace EpicProjects.View.CustomControls.Project
+{
+        class MenuItem : StackPanel
+        {
+                public StackPanel _upperPanel { get; set; }
+                public TextBlock _itemText{ get; set; }
+
+                public MenuItem (string text)
+	        {
+                        this.Orientation = Orientation.Vertical;
+                        this.Height = 50;
+                        this.Background = new SolidColorBrush(Colors.DarkMagenta);
+
+                        this._upperPanel = new StackPanel();
+                        _upperPanel.Height = 5;
+                        _upperPanel.Background = new SolidColorBrush(Color.FromRgb(121,85,72));
+
+
+                        this._itemText = new TextBlock();
+                        _itemText.Text = text;
+                        _itemText.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+                        _itemText.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+
+                        
+                        this.Children.Add(_itemText);
+                        this.Children.Add(_upperPanel);
+
+                        this.MouseEnter += MenuItem_MouseEnter;
+                        this.MouseLeave += MenuItem_MouseLeave;
+
+
+	        }
+
+                void MenuItem_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+                {
+                        this._upperPanel.Background = new SolidColorBrush(Color.FromRgb(121, 85, 72));
+                }
+
+                void MenuItem_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+                {
+                        this._upperPanel.Background = new SolidColorBrush(Color.FromRgb(33, 150, 243));
+                }
+        }
+}
---
+++ EpicProjects/EpicProjects/View/CustomControls/Project/MenuPanel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace EpicProjects.View.CustomControls.Project
+{
+        class MenuPanel : StackPanel
+        {
+
+                public MenuItem _brainStorming { get; set; }
+                public MenuItem _formations { get; set; }
+                public MenuItem _tasks { get; set; }
+                public MenuItem _maintenance { get; set; }
+
+                public MenuPanel(double widthAv)
+                {
+                        this.Orientation = Orientation.Horizontal;
+                        this.Background = new SolidColorBrush(Colors.Cyan);
+                        this.Height = 50;
+
+                        _brainStorming = new MenuItem("Brainstorming");
+                        _brainStorming.Width = widthAv / 4;
+                        _formations = new MenuItem("Formations");
+                        _formations.Width = widthAv / 4;
+                        _tasks = new MenuItem("Tasks");
+                        _tasks.Width = widthAv / 4;
+                        _maintenance = new MenuItem("Maintenance");
+                        _maintenance.Width = widthAv / 4;
+
+
+                        this.Children.Add(_brainStorming);
+                        this.Children.Add(_formations);
+                        this.Children.Add(_tasks);
+                        this.Children.Add(_maintenance);
+
+                }
+        }
+}
