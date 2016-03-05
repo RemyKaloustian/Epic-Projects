a5816ca6d5ef2178b81cb6ffb120ac839ea04c5c
RemyKaloustian <remy.kaloustian@gmail.com>
2016-03-05 18:36:41 +0100

    Adding the color constants, fix #62

--- EpicProjects/EpicProjects/Constants/Brushes.cs
+++ EpicProjects/EpicProjects/Constants/Brushes.cs
@@ -17,49 +17,94 @@ namespace EpicProjects.Constants
         public static class Brushes
         {
 
+
                 /*Repérages
-                 * //AVANT
-                 * Rouge = Pomegranate(242,38,19)
-                 * Cyan = Dodger blue(25,181,254
-                 * Bleu = San marino(68,108,179)
-                 * Vert foncé = Eucalyptus(38,166,91)
-                 * Vert Clair = Emerald(63,195,128)
-                 * Orange costaud = Ecstasy(249,105,14)
-                 * Orange léger = California (248,148,6);
-                 * Jaune léger = Confetti(233, 212, 96)
-                 * Jaune profond = SandStorm(249,191,59)
-                 * Magenta profond = SEANCE(154,18,179)
-                 * Magenta léger = Medium purple(191,85,236)
-                 * Violet profond = Rebeccapurple(102,51,153)
-                 * Violet léger = Studio (142,68,173)
-                 * Noir = Ebony clay (34,49,63)
-                 * Noir clair (lol) = Pickled bluewood (52,73,94) 
-                 * Gris clair = Edward(171,183,183)
-                 * Gris foncé = Lynch(108,122,137)
-                 * Rose foncé = Radical red(246,36,89)
-                 * Rose clair = Cabaret (210,82,127)
-                 * 
-                 * 
-                 * **********GOOGLE COLORS
-                 * 
-                 * Red(#f44336)
-                 * Pink(#e91e63)
-                 * Purple(#9c27b0)
-                 * Deep Purple(#673ab7)
-                 * Indigo(#3f51b5)
-                 * Blue(#2196f3)
-                 * Cyan(#00bcd4)
-                 * Teal(#009688)
-                 * Green(#4caf50)
-                 * Light green(#8bc34a)
-                 * Lime(#cddc39)
-                 * Yellow(#ffeb3b)
-                 * Amber(#ffc107)
-                 * Orange(#ff9800)
-                 * Deep orange(#ff5722)
-                 * Brown(#795548)
-                 * Grey(#9e9e9e)
-                 * Blue grey(#607d8b)
-                 * */
+             * //AVANT
+             * Rouge = Pomegranate(242,38,19)
+             * Cyan = Dodger blue(25,181,254
+             * Bleu = San marino(68,108,179)
+             * Vert foncé = Eucalyptus(38,166,91)
+             * Vert Clair = Emerald(63,195,128)
+             * Orange costaud = Ecstasy(249,105,14)
+             * Orange léger = California (248,148,6);
+             * Jaune léger = Confetti(233, 212, 96)
+             * Jaune profond = SandStorm(249,191,59)
+             * Magenta profond = SEANCE(154,18,179)
+             * Magenta léger = Medium purple(191,85,236)
+             * Violet profond = Rebeccapurple(102,51,153)
+             * Violet léger = Studio (142,68,173)
+             * Noir = Ebony clay (34,49,63)
+             * Noir clair (lol) = Pickled bluewood (52,73,94) 
+             * Gris clair = Edward(171,183,183)
+             * Gris foncé = Lynch(108,122,137)
+             * Rose foncé = Radical red(246,36,89)
+             * Rose clair = Cabaret (210,82,127)
+             * 
+             */
+            //FLAT COLORS
+            public static readonly string POMEGRANATE = "#f22613";
+            public static readonly string DODGER_BLUE = "#19b5fe";
+            public static readonly string SAN_MARINO = "#446cb3";
+            public static readonly string  EUCALYPTUS= "#26a65b";
+            public static readonly string  EMERALD= "#3fc380";
+            public static readonly string  ECSTASY= "#f9690e";
+            public static readonly string  CALIFORNIA= "#f89406";
+            public static readonly string  CONFETTI= "#e9d460";
+            public static readonly string  SANDSTORM= "#f9bf3b";
+            public static readonly string  SEANCE= "#9a12b3";
+            public static readonly string  MEDIUM_PURPLE= "#bf55ec";
+            public static readonly string  REBECCA_PURPLE= "#663399";
+            public static readonly string  STUDIO= "#8e44ad";
+            public static readonly string  EBONY_CLAY= "#22313f";
+            public static readonly string  PICKLED_BLUEWOOD= "#34495e";
+            public static readonly string  EDWARD= "#abb7b7";
+            public static readonly string  LYNCH= "#6c7a89";
+            public static readonly string  CABARET= "#d2527f";
+
+            //MATERIAL COLORS
+            public static readonly string  RED= "#f44336";
+            public static readonly string  PINK= "#e91e63";
+            public static readonly string  PURPLE= "#9c27b0";
+            public static readonly string  DEEP_PURPLE= "#673ab7";
+            public static readonly string  INDIGO= "#3f51b5";
+            public static readonly string  BLUE= "#2196f3";
+            public static readonly string  CYAN= "#0bcd4";
+            public static readonly string TEAL = "#009688";
+            public static readonly string GREEN = "#4caf50";
+            public static readonly string LIGHT_GREEN = "#8bc34a";
+            public static readonly string LIME = "#cddc39";
+            public static readonly string YELLOW = "#ffeb3b";
+            public static readonly string AMBER = "#ffc107";
+            public static readonly string ORANGE = "#ff9800";
+            public static readonly string DEEP_ORANGE = "#ff5722";
+            public static readonly string BROWN = "#795548";
+            public static readonly string GREY = "#9e9e9e";
+            public static readonly string BLUE_GREY = "#607d8b";
+
+
+
+
+            
+            /* **********GOOGLE COLORS
+             * 
+             * Red(#f44336)
+             * Pink(#e91e63)
+             * Purple(#9c27b0)
+             * Deep Purple(#673ab7)
+             * Indigo(#3f51b5)
+             * Blue(#2196f3)
+             * Cyan(#00bcd4)
+             * Teal(#009688)
+             * Green(#4caf50)
+             * Light green(#8bc34a)
+             * Lime(#cddc39)
+             * Yellow(#ffeb3b)
+             * Amber(#ffc107)
+             * Orange(#ff9800)
+             * Deep orange(#ff5722)
+             * Brown(#795548)
+             * Grey(#9e9e9e)
+             * Blue grey(#607d8b)
+             * */
         }//class Brushes
 }//ns
