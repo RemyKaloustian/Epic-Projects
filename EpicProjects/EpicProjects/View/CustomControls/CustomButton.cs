588396b2310bb3571f1178529ed8467a0a7d1d68
RemyKaloustian <remy.kaloustian@gmail.com>
2016-03-09 07:52:12 +0100

    Closing some bookmarks, fix #91

---
+++ EpicProjects/EpicProjects/View/CustomControls/CustomButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace EpicProjects.View.CustomControls
+{
+        public class CustomButton : StackPanel
+        {
+                public TextBlock _block { get; set; }
+
+                public CustomButton(string content, )
+                {
+
+                }
+        }
+}
