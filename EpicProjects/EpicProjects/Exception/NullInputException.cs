ef413f28de78db82c7faf5861a12a35e1819b818
RemyKaloustian <remy.kaloustian@gmail.com>
2016-03-09 20:13:12 +0100

    Adding the null input exception , fix #101

---
+++ EpicProjects/EpicProjects/Exception/NullInputException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicProjects.Exception
+{
+        public class NullInputException
+        {
+                //NullInputWindow wind = new NullInputWindow();
+                //wind.Sow();
+        }//class NullInputException
+}//ns
