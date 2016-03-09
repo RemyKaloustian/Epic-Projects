5759118ff3219074107ab371d2fae1f09e138160
RemyKaloustian <remy.kaloustian@gmail.com>
2016-03-09 20:08:30 +0100

    Adding shortcuts constants, fix #99, fix #98

---
+++ EpicProjects/EpicProjects/Constants/Tooltips.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicProjects.Constants
+{
+        public class Tooltips
+        {
+                public static readonly string SETTINGS = "Settings";
+                public static readonly string OPEN = "Open project";
+                public static readonly string NEW = "New project";
+                public static readonly string DOC = "Documentation";
+                public static readonly string BUG = "Report bug";
+        }//class Tooltips
+}//ns
