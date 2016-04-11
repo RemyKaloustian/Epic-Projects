26090da8c9eaef2b5d5f1c8f99199274d78d22f4
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-11 16:50:43 +0200

    Changing structure, adding folders in Constants, fix #259

---
+++ EpicProjects/EpicProjects/Constants/Model/Priorities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicProjects.Constants
+{
+        public class Priorities
+        {
+                public static readonly string NOT_IMPORTANT_CODE = "1";
+                public static readonly string LESS_IMPORTANT_CODE = "2";
+                public static readonly string IMPORTANT_CODE = "3";
+                public static readonly string ULTRA_IMPORTANT_CODE = "4";
+                public static readonly string MOST_IMPORTANT_CODE = "5";
+
+
+                public static readonly string NOT_IMPORTANT = "Not important";
+                public static readonly string LESS_IMPORTANT = "Less important";
+                public static readonly string IMPORTANT = "Important";
+                public static readonly string ULTRA_IMPORTANT = "Ultra important";
+                public static readonly string MOST_IMPORTANT = "Most important thing in life";
+        }//class Priorities
+}//ns
