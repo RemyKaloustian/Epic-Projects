2ce5258ad921f84e9cf0b27b3798e386cb4aba67
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-04-18 06:36:03 +0200

    Making the Theme Selector

---
+++ EpicProjects/EpicProjects/View/Theme/ThemeSelector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicProjects.View.Theme
+{
+        public class ThemeSelector
+        {
+                public static Theme  _theme{ get; set; }
+        }//class ThemeSelector
+}//ns
