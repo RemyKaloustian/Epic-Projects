f8837f2296ca8d89f0f1eebdcf74ab3680c58d57
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-17 08:44:08 +0200

    fix #398

---
+++ EpicProjects/EpicProjects/Constants/Model/Sorting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicProjects.Constants.Model
+{
+        public static class Sorting
+        {
+                public static readonly string IMPORTANCE = "importance";
+                public static readonly string STATE = "state";
+                public static readonly string CREATION = "creation";
+
+
+        }//class Sorting()
+}//ns
