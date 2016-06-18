c1e6ae947629dfdab17c8533644b7a2aa835f197
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-18 16:18:28 +0200

    fix #433

---
+++ EpicProjects/EpicProjects/View/CustomControls/PopUp/OverallStatsPopUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicProjects.View.CustomControls.PopUp
+{
+        public class OverallStatsPopUp : PopUp
+        {
+                 public OverallStatsPopUp(double width, double height, string content)
+                        : base(width, height, content)
+                {
+
+                }//ctor()
+        }//class OverallStatsPopUp()
+}//ns
--- EpicProjects/EpicProjects/View/CustomControls/PopUp/StatsPopUp.cs
+++ EpicProjects/EpicProjects/View/CustomControls/PopUp/StatsPopUp.cs
@@ -22,6 +22,7 @@ namespace EpicProjects.View.CustomControls.PopUp
                
 
                         _overallButton = new ValidateButton("Overall stats", this.Width * 0.5, this.Height * 0.07, new System.Windows.Thickness(0, 14, 0, 3), new System.Windows.Thickness(5, 5, 5, 5), System.Windows.HorizontalAlignment.Center);
+                        _overallButton.MouseDown += _overallButton_MouseDown;
 
                         _closeButton = new CancelButton(ControlsValues.CLOSE, this.Width * 0.5, this.Height * 0.07, new System.Windows.Thickness(0, 2, 0, 3), new System.Windows.Thickness(5, 5, 5, 5), System.Windows.HorizontalAlignment.Center);
 
@@ -30,6 +31,12 @@ namespace EpicProjects.View.CustomControls.PopUp
 
                         _container.Children.Add(_overallButton);
                         _container.Children.Add(_closeButton);
+                }
+
+                void _overallButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+                {
+                        this.Close();
+                        OverallStatsPopUp osp = new OverallStatsPopUp(Dimensions.GetWidth() * 0.5, Dimensions.GetHeight() * 0.8, "Overall Statistics");
                 }//ctor()
 
                 void _closeButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
