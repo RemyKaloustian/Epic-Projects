5302bdf2c2dcef22830be3726458d08deb0571fb
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-19 08:29:36 +0200

    fix #455, fix #447

---
+++ EpicProjects/EpicProjects/View/CustomControls/Panels/StatsPanel.cs
@@ -0,0 +1,32 @@
+﻿using EpicProjects.View.CustomControls.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace EpicProjects.View.CustomControls.Panels
+{
+        public class StatsPanel : StackPanel
+        {
+                public TextBlock _number{ get; set; }
+                public TextBlock _text{ get; set; }
+
+                public StatsPanel(string number, string text)
+                {
+                        this.Orientation = System.Windows.Controls.Orientation.Horizontal;
+                        this.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+                        _number = new NumberBlock();
+                        _number.Text = number;
+
+                        _text = new StatsBlock();
+                        _text.Text = text;
+                        _text.VerticalAlignment = System.Windows.VerticalAlignment.Bottom;
+
+                        this.Children.Add(_number);
+                        this.Children.Add(_text);
+                }//ctor
+
+        }//class StatsPanel
+}//ns
--- EpicProjects/EpicProjects/View/CustomControls/PopUp/ProjectStatsPopUp.cs
+++ EpicProjects/EpicProjects/View/CustomControls/PopUp/ProjectStatsPopUp.cs
@@ -1,4 +1,6 @@
 ﻿using EpicProjects.Constants;
+using EpicProjects.Database;
+using EpicProjects.View.CustomControls.Panels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,8 @@ namespace EpicProjects.View.CustomControls.PopUp
         {
                 public Separator _separator { get; set; }
 
+                public StackPanel _brainstormingsPanel { get; set; }
+
                 public ProjectStatsPopUp(double width, double height, string content, string project)
                         : base(width, height, content)
                 {
@@ -19,7 +23,12 @@ namespace EpicProjects.View.CustomControls.PopUp
                         _separator.Width = this.Width * 0.6;
                         _separator.Background = Palette2.GetColor(Palette2.THIN_GRAY);
 
+
+                        _brainstormingsPanel = new StatsPanel(new Selector("tamaire").SelectBrainstormings(project).Count.ToString(), "  Brainstormings");
+
+
                         _container.Children.Add(_separator);
+                        _container.Children.Add(_brainstormingsPanel);
                 }
         }//class ProjectsStatsPopUp
 }//ns
